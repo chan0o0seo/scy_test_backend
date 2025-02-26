00e63297ba76c9bd2f5f734f4c6801471fa8209d
scy <tjcksdud0221@naver.com>
2025-02-26 10:19:09 +0900

    Comment Entity 추가

--- src/main/java/com/example/scy_test_backend/comment/model/Comment.java
+++ src/main/java/com/example/scy_test_backend/comment/model/Comment.java
@@ -1,4 +1,22 @@
 package com.example.scy_test_backend.comment.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    private String content;
+
+    private String writer;
 }
