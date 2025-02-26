7d314289a2c245218db66a5fe9971aa769260300
scy <tjcksdud0221@naver.com>
2025-02-26 15:56:12 +0900

    swwager 추가

--- src/main/java/com/example/scy_test_backend/config/SwaggerConfig.java
+++ src/main/java/com/example/scy_test_backend/config/SwaggerConfig.java
@@ -1,15 +1,27 @@
 package com.example.scy_test_backend.config;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(
-                title = "My API",
-                version = "v1",
-                description = "API documentation for my app"
-        )
-)
 public class SwaggerConfig {
+
+        @Bean
+        public GroupedOpenApi boardGroupedOpenApi() {
+                return GroupedOpenApi
+                        .builder()
+                        .group("board") // group 설정 (API들을 그룹화시켜 그룹에 속한 API들만 확인할 수 있도록 도와줌)
+                        .pathsToMatch("/board/**") // group에 포함될 API endpoint 경로
+                        .addOpenApiCustomizer(openApi ->
+                                openApi.setInfo(new Info()
+                                        .title("board api") // API 제목
+                                        .description("게시판 업무 처리를 위한 API") // API 설명
+                                        .version("1.0.0") // API 버전
+                                )
+                        )
+                        .build();
+        }
 }
