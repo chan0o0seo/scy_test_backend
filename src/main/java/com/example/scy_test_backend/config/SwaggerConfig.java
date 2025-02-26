cbbd9859bd68aa18a2bdb724168765c6a3d8e2a2
scy <tjcksdud0221@naver.com>
2025-02-26 09:45:56 +0900

    BaseResponse 추가 및 swagger 설정

---
+++ src/main/java/com/example/scy_test_backend/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.example.scy_test_backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "My API", version = "1.0", description = "API Documentation")
+)
+public class SwaggerConfig {
+}
