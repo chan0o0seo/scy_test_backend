9d6915d00ceab8d1f178b9c5e9050ddd48e9156f
scy <tjcksdud0221@naver.com>
2025-02-26 10:39:11 +0900

    Comment Request dto 생성

--- src/main/java/com/example/scy_test_backend/comment/model/CommentDto.java
+++ src/main/java/com/example/scy_test_backend/comment/model/CommentDto.java
@@ -1,4 +1,15 @@
 package com.example.scy_test_backend.comment.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class CommentDto {
+
+    @Getter
+    @Setter
+    public static class RequestDto {
+        private long board_idx;
+        private String content;
+        private String writer;
+    }
 }
